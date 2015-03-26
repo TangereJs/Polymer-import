b35a76b22810dd12034329fc835f3ec8b82ccd9b
Martin Amm <martin.amm@adenin.com>
2015-03-26 13:53:47 -0400

    added option to check if an custom element was already imported in Chrome if the same element is imported twice the element seems to be broken after the 2nd import, so we now check before starting an import strange: in IE/FF with the polyfill that problem doesn't exist

--- polymer-import.js
+++ polymer-import.js
@@ -7,73 +7,92 @@
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
 
-(function(scope) {
+(function (scope) {
 
-/**
- * @class Polymer
- */
+  /**
+   * @class Polymer
+   */
 
 
-/**
- * Loads the set of HTMLImports contained in `node`. Notifies when all
- * the imports have loaded by calling the `callback` function argument.
- * This method can be used to lazily load imports. For example, given a 
- * template:
- *     
- *     <template>
- *       <link rel="import" href="my-import1.html">
- *       <link rel="import" href="my-import2.html">
- *     </template>
- *
- *     Polymer.importElements(template.content, function() {
- *       console.log('imports lazily loaded'); 
- *     });
- * 
- * @method importElements
- * @param {Node} node Node containing the HTMLImports to load.
- * @param {Function} callback Callback called when all imports have loaded.
- */
-function importElements(node, callback) {
-  if (node) {
-    document.head.appendChild(node);
-    HTMLImports.whenReady(callback);
-  } else if (callback) {
-    callback();
+  /**
+   * Loads the set of HTMLImports contained in `node`. Notifies when all
+   * the imports have loaded by calling the `callback` function argument.
+   * This method can be used to lazily load imports. For example, given a
+   * template:
+   *
+   *     <template>
+   *       <link rel="import" href="my-import1.html">
+   *       <link rel="import" href="my-import2.html">
+   *     </template>
+   *
+   *     Polymer.importElements(template.content, function() {
+   *       console.log('imports lazily loaded');
+   *     });
+   *
+   * @method importElements
+   * @param {Node} node Node containing the HTMLImports to load.
+   * @param {Function} callback Callback called when all imports have loaded.
+   */
+  function importElements(node, callback) {
+    if (node) {
+      document.head.appendChild(node);
+      HTMLImports.whenReady(callback);
+    } else if (callback) {
+      callback();
+    }
   }
-}
 
-/**
- * Loads an HTMLImport for each url specified in the `urls` array.
- * Notifies when all the imports have loaded by calling the `callback` 
- * function argument. This method can be used to lazily load imports. 
- * For example,
- *
- *     Polymer.import(['my-import1.html', 'my-import2.html'], function() {
- *       console.log('imports lazily loaded'); 
- *     });
- * 
- * @method import
- * @param {Array} urls Array of urls to load as HTMLImports.
- * @param {Function} callback Callback called when all imports have loaded.
- */
-function _import(urls, callback) {
-  if (urls && urls.length) {
+  /**
+   * Loads an HTMLImport for each url specified in the `urls` array.
+   * Notifies when all the imports have loaded by calling the `callback`
+   * function argument. This method can be used to lazily load imports.
+   * For example,
+   *
+   *     Polymer.import(['my-import1.html', 'my-import2.html'], function() {
+   *       console.log('imports lazily loaded');
+   *     });
+   *
+   * @method import
+   * @param {Array} urls Array of urls to load as HTMLImports.
+   * @param {Function} callback Callback called when all imports have loaded.
+   */
+  function _import(urls, callback) {
+    if (urls && urls.length) {
       var frag = document.createDocumentFragment();
-      for (var i=0, l=urls.length, url, link; (i<l) && (url=urls[i]); i++) {
+      for (var i = 0, l = urls.length, url, link;
+        (i < l) && (url = urls[i]); i++) {
         link = document.createElement('link');
         link.rel = 'import';
         link.href = url;
         frag.appendChild(link);
       }
       importElements(frag, callback);
-  } else if (callback) {
-    callback();
+    } else if (callback) {
+      callback();
+    }
   }
-}
 
-// exports
-scope.import = _import;
-scope.importElements = importElements;
+  // exports
+  scope.import = _import;
+  scope.importElements = importElements;
 
 })(Polymer);
 
+
+// native importing elements twice causes errors (when not using the import polyfill), so we're checking if it's already imported
+// queries Polymer.telemetry.registrations which seems hacky
+
+(function (scope) {
+  function _isImported(elementName) {
+    var regs = Polymer.telemetry.registrations;
+
+    for (i = 0; i < regs.length; i++) {
+      var reg = regs[i];
+      if (reg.is == elementName) return true;
+    }
+
+    return false;
+  }
+
+  scope.import.isImported = _isImported;
+})(Polymer);
